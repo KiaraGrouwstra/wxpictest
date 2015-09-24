d30e6f50dcb2d5e26f97402e423112cf9c5f974d
Tycho Grouwstra <tychogrouwstra@gmail.com>
2015-09-24 06:10:50 +0000

    pic, email

--- hi.rb
+++ hi.rb
@@ -1,11 +1,25 @@
 require 'sinatra'
+require 'mail'
 
 set :bind, '0.0.0.0'
 # set :port, 9494
 
-get '/hi' do
+get '/awesome.jpg' do
   #s = request.referer
   s = "#{request.env}"
-  puts s
-  s
+  # puts s
+  # s
+  email s
+  send_file('./awesome.jpg', :disposition => 'inline')
+end
+
+def email(str)
+  mail = Mail.new do
+    from     'wxtest@ruby.gem'
+    to       'tychogrouwstra@gmail.com'
+    subject  'wx headers'
+    body     str
+  end
+  mail.delivery_method :sendmail
+  mail.deliver
 end
