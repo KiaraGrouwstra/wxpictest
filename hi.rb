0ec1adc7c04f05074c877a0a8a83efae3c6417a7
Tycho Grouwstra <tychogrouwstra@gmail.com>
2015-09-24 06:29:15 +0000

    try

--- hi.rb
+++ hi.rb
@@ -7,19 +7,24 @@ set :bind, '0.0.0.0'
 get '/awesome.jpg' do
   #s = request.referer
   s = "#{request.env}"
-  # puts s
+  puts s
   # s
-  email s
+  # email s
   send_file('./awesome.jpg', :disposition => 'inline')
 end
 
 def email(str)
+  # Mail.defaults do
+  #   smtp '127.0.0.1' # Port 25 defult
+  # end
   mail = Mail.new do
-    from     'wxtest@ruby.gem'
-    to       'tychogrouwstra@gmail.com'
+    #from     'tychogrouwstra@gmail.com'
+    #to       'tychogrouwstra@gmail.com'
+    from     'tycho01@mailinator.com'
+    to       'tycho01@mailinator.com'
     subject  'wx headers'
     body     str
   end
   mail.delivery_method :sendmail
-  mail.deliver
+  mail.deliver!
 end
